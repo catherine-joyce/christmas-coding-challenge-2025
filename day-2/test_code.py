d1974f50c34fe87da1e8dabf02a6bf6337678e47
catherine-joyce <cannejoyce@outlook.com>
2025-12-01 13:51:52 +0000

    added day 2 problem

---
+++ day-2/test_code.py
@@ -0,0 +1,14 @@
+from solution import Solution
+
+def test_friends_134_outputs_314():
+    solution = Solution()
+    order = [3,1,2,5,4]
+    friends = [1,3,4]
+    assert solution.recoverOrder(order, friends) == [3,1,4]
+
+def test_friends_25_outputs_52():
+    solution = Solution()
+    order = [1,4,5,3,2]
+    friends = [2,5]
+    assert solution.recoverOrder(order, friends) == [5,2]
+
