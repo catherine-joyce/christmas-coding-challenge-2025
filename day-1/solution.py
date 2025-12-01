ece60ec316f61ea55129aa6dbcbcd3636dc15bf6
catherine-joyce <cannejoyce@outlook.com>
2025-12-01 12:27:05 +0000

    day 1 problem solved

---
+++ day-1/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        num = int(''.join([str(element) for element in digits]))
+        num += 1
+        return [int(element) for element in list(str(num))]
---
+++ day-1/test_code.py
@@ -0,0 +1,9 @@
+from solution import Solution
+
+def test_123_124():
+    solution = Solution()
+    assert solution.plusOne([1,2,3]) == [1,2,4]
+
+def test_9_10():
+    solution = Solution()
+    assert solution.plusOne([9]) == [1,0]
