34cd420c3414d012feff4f44a9df707a8b797450
catherine-joyce <cannejoyce@outlook.com>
2025-12-05 15:19:50 +0000

    day 5 problem solved

---
+++ day-5/test_code.py
@@ -0,0 +1,19 @@
+from solution import Solution
+
+def test_reverse_letters():
+    solution = Solution()
+    s = "ab-cd"
+    answer = solution.reverseOnlyLetters(s)
+    assert answer == "dc-ba"
+
+def test_reverse_letters_case_2():
+    solution = Solution()
+    s = "Test1ng-Leet=code-Q!"
+    answer = solution.reverseOnlyLetters(s)
+    assert answer == "Qedo1ct-eeLg=ntse-T!"
+
+def test_reverse_letters_case_3():
+    solution = Solution()
+    s = "7_28]"
+    answer = solution.reverseOnlyLetters(s)
+    assert answer == "7_28]"
