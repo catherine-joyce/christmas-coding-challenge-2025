dd91813007783b189a99f68076c70c462f93021c
catherine-joyce <cannejoyce@outlook.com>
2025-12-06 11:35:51 +0000

    solved problem 3

---
+++ day-6/problem-3/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        string_list = []
+        i = 0
+        for j in range(len(nums)):
+            if (j == len(nums)-1) or (nums[j] + 1 != nums[j+1]):
+                if i == j:
+                    string_list.append(str(nums[i]))
+                else:
+                    string_list.append(str(nums[i]) + "->" + str(nums[j]))
+                i = j + 1
+        return string_list
+
+
+
+
---
+++ day-6/problem-3/test_code.py
@@ -0,0 +1,15 @@
+from solution import Solution
+
+
+def test_generate_ranges():
+    solution = Solution()
+    nums = [0, 1, 2, 4, 5, 7]
+    answer = solution.summaryRanges(nums)
+    assert answer == ["0->2", "4->5", "7"]
+
+
+def test_generate_ranges_2():
+    solution = Solution()
+    nums = [0, 2, 3, 4, 6, 8, 9]
+    answer = solution.summaryRanges(nums)
+    assert answer == ["0", "2->4", "6", "8->9"]
