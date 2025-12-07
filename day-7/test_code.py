5474fca9eba8de69a3e98e3741c947ab66b35966
catherine-joyce <cannejoyce@outlook.com>
2025-12-07 00:24:55 +0000

    solved problem 1

---
+++ day-7/test_code.py
@@ -0,0 +1,14 @@
+from solution import Solution
+
+
+def test_last_word_5():
+    solution = Solution()
+    s = "Hello World"
+    answer = solution.lengthOfLastWord(s)
+    assert answer == 5
+
+def test_last_word_4():
+    solution = Solution()
+    s = "   fly me   to   the moon  "
+    answer = solution.lengthOfLastWord(s)
+    assert answer == 4
