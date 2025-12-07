03760568f32e94b3b57351893fdb0d7163ed344d
catherine-joyce <cannejoyce@outlook.com>
2025-12-07 07:05:53 +0000

    refactoring

--- script.sh
+++
@@ -1,6 +0,0 @@
-# Rename all test_code.py files to include their parent directory name
-find . -name "test_code.py" -type f | while read file; do
-    dir=$(dirname "$file")
-    dirname_base=$(basename "$dir")
-    mv "$file" "$dir/test_${dirname_base}.py"
-done
