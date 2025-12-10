c1463d63c7a11bc1f430f5a8b75ccd8ee027220e
catherine-joyce <cannejoyce@outlook.com>
2025-12-10 11:11:28 +0000

    refactoring

--- day-4/problem-1/day4_problem1_solution.py
+++ day-4/problem-1/day4_problem1_solution.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
         low = 0
         high = len(nums) - 1
         found = False
