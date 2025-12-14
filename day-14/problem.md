e545c677f88881832257e4e4db536a65e07b2224
catherine-joyce <cannejoyce@outlook.com>
2025-12-14 19:02:15 +0000

    added tests

---
+++ day-14/problem.md
@@ -0,0 +1,3 @@
+# Palindrome number
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
--- day-14/test_day14.py
+++ day-14/test_day14.py
@@ -5,3 +5,15 @@ def test_palindrome_121():
     x = 121
     result = solution.isPalindrome(x)
     assert result == True
+
+def test_palindrome_negative_121():
+    solution = Solution()
+    x = -121
+    result = solution.isPalindrome(x)
+    assert result == False
+
+def test_palindrome_10():
+    solution = Solution()
+    x = 10
+    result = solution.isPalindrome(x)
+    assert result == False
