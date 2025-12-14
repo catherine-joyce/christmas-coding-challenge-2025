58011fb85877a9ab670ed3a86231d618af2dfae8
catherine-joyce <cannejoyce@outlook.com>
2025-12-14 19:00:46 +0000

    solved problem

---
+++ day-14/day14_solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        int_string = str(x)
+        i = 0
+        j = len(int_string) - 1
+        while i < j:
+            if int_string[i] == int_string[j]:
+                i += 1 
+                j -= 1
+            else:
+                return False
+        return True
---
+++ day-14/test_day14.py
@@ -0,0 +1,7 @@
+from day14_solution import Solution
+
+def test_palindrome_121():
+    solution = Solution()
+    x = 121
+    result = solution.isPalindrome(x)
+    assert result == True
